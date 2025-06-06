3d8bd7b7e721ccba24331865796edbe61a40f08e
Nelson Tsa <t.nelson@hotmail.fr>
2025-06-06 20:55:24 +0200

    test againand again

--- frontend/src/app/services/userservice.service.ts
+++ frontend/src/app/services/userservice.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -9,7 +10,7 @@ export class UserService {
 
   getCurrentUser(): Observable<User> {
     const token = localStorage.getItem('token');
-    const apiUrl = window.apiConfig?.apiUrl || 'http://localhost:8080';
+    const apiUrl = environment.apiUrl;
     return this.http.get<User>(`${apiUrl}/api/me`, {
       headers: {
         Authorization: `Bearer ${token}`,
